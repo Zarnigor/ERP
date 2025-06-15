dbe841aab74997e14b3dfc4bc4ff1730fa4932f8
Zarnigor Zokirmatova <zarnigorn1255@gmail.com>
2025-06-15 22:21:41 +0500

    Update settings.py

--- clothes_erp/settings.py
+++ clothes_erp/settings.py
@@ -85,9 +85,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
